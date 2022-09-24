7a89be3ec3c318183e98f2bd617152306a82817b
msnsone <sansone.marcelo@gmail.com>
2022-09-24 14:06:56 -0300

    mudança folder

--- Exercícios/0309/ex1.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main ()
-{
-	int n;
-	printf ("Digite um valor inteiro:");
-	scanf ("%i", &n);
-	printf ("o valor digitado foi %i", n);
-	return 0;
-}
--- Exercícios/0309/ex2.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main ()
-{
-	float n;
-	printf ("Digite um valor real:");
-	scanf ("%f", &n);
-	printf ("o valor digitado foi %f", n);
-	return 0;
-}
